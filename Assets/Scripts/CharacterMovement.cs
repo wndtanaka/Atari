784fbb1f1aa8d6552d14f7f71352f7833bf2b49b
Unknown <wndtanaka@gmail.com>
2017-09-05 21:21:04 +1000

    added bits

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     public float movementSpeed = 15f;
     public float jumpSpeed = 25f;
 
@@ -16,27 +14,6 @@ public class CharacterMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-=======
-
-    public float movementSpeed = 15f;
-    public float jumpSpeed = 25f;
-
-=======
-
-    public float movementSpeed = 15f;
-    public float jumpSpeed = 25f;
-
->>>>>>> Stashed changes
-    private float jumpTime = 0f;
-    private Rigidbody2D rigid;
-    // Use this for initialization
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody2D>();
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -48,8 +25,6 @@ public class CharacterMovement : MonoBehaviour
             transform.eulerAngles = new Vector2(0, 180);
         }
         if (Input.GetKey(KeyCode.D))
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         {
             transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 0);
@@ -57,23 +32,9 @@ public class CharacterMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > canJump)
         {
             rb.velocity = new Vector2(0, jumpSpeed);
-
             canJump = Time.time + 0.5f;
-=======
-=======
->>>>>>> Stashed changes
-        {
-            transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.Space)&& Time.time > jumpTime)
-        {
-            rigid.velocity = new Vector2(0, jumpSpeed);
-            jumpTime = Time.time + 0.5f;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         }
     }
 }
+
+
